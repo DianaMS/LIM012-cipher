bf0513036e08cb86ffeb263e9807c9efcdf4c733
DianaMS <dmartinezs@uni.pe>
2020-04-10 00:47:21 -0500

    estilos y responsive pagina2

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const siguiente = document.getElementById('siguiente');
 const pag1 = document.getElementById('pag1');
 const pag2 = document.getElementById('pag2');
 const contenedor = document.getElementById('contenedor');
-pag2.classList.add('hide');
+pag2.style.display='none';
 
 siguiente.addEventListener('click', () => {
     if (nombre.value && telefono.value && correo.value !== '') {
         pag1.style.display= 'none';
-        pag2.classList.remove('hide');
+        pag2.style.display='block';
     } else {
         contenedor.innerHTML = '* Todos los campos son obligatorios';
     }
