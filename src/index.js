10ffaa3b2d75bfa2fba17e7a3a0006dc03780063
DianaMS <dmartinezs@uni.pe>
2020-04-09 21:23:41 -0500

    agregar función al boton ->

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
+const nombre = document.getElementById('nombre');
+const telefono = document.getElementById('telefono');
+const correo = document.getElementById('correo');
+const siguiente = document.getElementById('siguiente');
+const pag1 = document.getElementById('pag1');
+const pag2 = document.getElementById('pag2');
+const contenedor = document.getElementById('contenedor');
+pag2.classList.add('hide');
+
+siguiente.addEventListener('click', () => {
+    if (nombre.value && telefono.value && correo.value !== '') {
+        pag1.style.display= 'none';
+        pag2.classList.remove('hide');
+    } else {
+        contenedor.innerHTML = '* Todos los campos son obligatorios';
+    }
+});
+
+
 console.log(cipher);
